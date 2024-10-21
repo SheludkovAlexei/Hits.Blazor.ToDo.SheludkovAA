4cf4f26b3e93aa053f4da6317a42df92dca45b86
SheludkovAlexei <144366359+SheludkovAlexei@users.noreply.github.com>
2024-10-21 16:18:58 +0500

    Изменения Лекция 6

--- TodoServerApp/Data/Services/MemoryDataService.cs
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -10,10 +10,26 @@ namespace TodoServerApp.Data.Services
             new() {Id = 2, Title = "Задача 2", Description = "Описание задачи 2", CreatedDate = DateTime.Now, FinishDate = DateTime.Now.AddDays(5)},
             new() {Id = 3, Title = "Задача 3", Description = "Описание задачи 3", CreatedDate = DateTime.Now, FinishDate = DateTime.Now.AddDays(34)}
         ];
-        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskItem> GetTaskAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllAsync()
         {
             await Task.Delay(1000);
             return await Task.FromResult(tasks);
+        }
+
+        public Task SaveAsync(TaskItem item)
+        {
+            throw new NotImplementedException();
         }
     }
 }
